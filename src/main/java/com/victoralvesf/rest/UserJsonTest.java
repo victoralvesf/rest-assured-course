a6b48104cfbfcc5caf6e6eca3b51c6974eccdbfd
Victor Alves <viictor.alvess12@gmail.com>
2024-01-04 14:35:14 -0300

    add more tests for users resource

--- src/main/java/com/victoralvesf/rest/UserJsonTest.java
+++ src/main/java/com/victoralvesf/rest/UserJsonTest.java
@@ -1,6 +1,10 @@
 package com.victoralvesf.rest;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -73,5 +77,50 @@ public class UserJsonTest {
                 .body("age[1]", is(25))
                 .body("filhos[2].name", hasSize(2))
                 .body("filhos[2].name", hasItems("Zezinho", "Luizinho"));
+    }
+
+    @Test
+    public void shouldMakeAdvancedValidations() {
+        given()
+                .when()
+                .get("https://restapi.wcaquino.me/users")
+                .then()
+                .statusCode(200)
+                .body("$", hasSize(3))
+                .body("age.findAll{it <= 25}.size()", is(2))
+                .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+                .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+                .body("findAll{it.age <= 25 && it.age > 20}.name[0]", is("Maria Joaquina"))
+                .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+                .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+                .body("find{it.age > 20}.name", is("João da Silva"))
+                .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+                .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+                .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+                .body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+                .body("age.collect{it * 2}", hasItems(60, 50, 40))
+                .body("id.max()", is(3))
+                .body("salary.min()", is(1234.5678f))
+                .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+                .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)));
+    }
+
+    @Test
+    public void shouldExtractAndUseJavaToValidate() {
+        Object response = given()
+                .when()
+                .get("https://restapi.wcaquino.me/users")
+                .then()
+                .statusCode(200)
+                .extract().path("name.findAll{it.startsWith('Maria')}");
+
+        @SuppressWarnings("unchecked")
+        ArrayList<String> names = new ArrayList<>((ArrayList<String>) response);
+
+        Assert.assertEquals(1, names.size());
+        Assert.assertTrue(names.get(0).equalsIgnoreCase("mArIa Joaquina"));
+        Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());
     }
 }
